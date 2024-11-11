72587409bc5f09b5cfe20dff6f07fa9fff56a4d5
mykeels <mykehell123@gmail.com>
2024-11-11 07:33:56 -0500

    feat: use super-imposition

--- scripts/process-songs.script.ts
+++ scripts/process-songs.script.ts
@@ -3,25 +3,36 @@ import path from "path";
 
 const styleHtml = `
 <style>
-    div,pre {
+    .tonic-solfa {
+        font-weight: bold;
+        position: relative;
+        padding-bottom: 1em;
+        margin-top: 0.5em;
         font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
         font-size: 2em;
         letter-spacing: 0.2em;
-        padding: 0;
-        margin: 0;
-    }
-    .tonic-solfa {
-        font-weight: bold;
-        color: #FA0;
     }
     .solfa {
-        letter-spacing: -0.1em;
-        width: 1em;
+      color: #FA0;
+      font-size: 0.8em;
+      vertical-align: bottom;
+      margin-bottom: 1.2em;
+      display: inline-block;
+      margin-left: -0.6em;
+      position: absolute;
+      top: -0.9em;
     }
-    .space {
-        display: inline-block;
+    .song {
+      padding-top: 1em;
+      padding-bottom: 1em;
+    }
+    .break {
+      height: 2em;
+      display: block;
     }
 </style>
+
+<div class="song">
 `;
 
 const songsDir = path.join(__dirname, "../songs");
@@ -32,23 +43,19 @@ const songFilePaths = fs.readdirSync(songsDir);
 for (const songFilePath of songFilePaths) {
   const songLines = fs.readFileSync(path.join(songsDir, songFilePath), "utf8").split("\n");
   const html: string[] = [styleHtml];
+  let songLine = '';
+  let tonicSolfa = '';
   for (const line of songLines) {
     if (line.startsWith(">")) {
-      html.push(`<div class="tonic-solfa">\n${
-          line.slice(1)
-            .trim()
-            .split(" ")
-            .map(solfa => solfa === '' ? '' : `<span class="solfa">${
-              `${solfa.slice(0, 1)}${solfa.length > 1 ? `<sup>${solfa.slice(1)}</sup>` : ''}`
-            }</span>\n`)
-            .join("<span class='space'>&nbsp;</span>")
-        }</div>`);
+      tonicSolfa = line.slice(1);
+    } else if (line.trim()) {
+      songLine = line;
+      html.push(superimpose(songLine, tonicSolfa));
     } else {
-      html.push(`<div class="line">${
-        line.trim().replaceAll(" ", "<span class='space'>&nbsp;</span>")
-      }</div>`);
+      html.push(line + "<div class='break'><hr></div>");
     }
   }
+  html.push("</div>");
   const outputFilePath = path.join(outputDir, songFilePath.replace(".tonicsolfa", ".html"));
   fs.mkdirSync(outputDir, { recursive: true });
   fs.writeFileSync(outputFilePath, html.join("\n"));
@@ -58,6 +65,8 @@ console.log("Done!");
 
 function superimpose(line: string, tonicSolfa: string) {
   const result = [];
+  line = line.trim();
+  tonicSolfa = tonicSolfa.trim();
   for (let i = 0; i < line.length; i++) {
     const char = line[i];
     let solfa = getSolfa(i);
@@ -68,12 +77,14 @@ function superimpose(line: string, tonicSolfa: string) {
     }
   }
 
-  return result.join("");
+  return `<div class="tonic-solfa">${result.join("")}</div>`;
 
   function getSolfa(i: number) {
     let solfa = tonicSolfa[i];
     if (solfa === ' ') {
       return solfa;
+    } else if (solfa === "'") {
+      return ' ';
     }
     while (tonicSolfa[i + 1] && tonicSolfa[i + 1] !== ' ') {
       solfa += tonicSolfa[i + 1];
